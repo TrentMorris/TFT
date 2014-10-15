fb689a5ed4cb1cf605543858404ba5d5c4596265
Trent Morris <trent.morris@banno.com>
2014-10-15 10:48:54 -0500

    committing file

---
+++ sbt.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+
+dirName = ARGV[0]
+
+FileUtils::mkdir_p dirName + '/src/main/scala'
+FileUtils::mkdir_p dirName + '/src/test/scala'
+FileUtils::mkdir_p dirName + '/project'
+buildFile = File.new(dirName + "/build.sbt", "w")
+buildFile.puts(%Q[name := "#{dirName}"
+
+version:= "1.0"
+
+resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+scalaVersion := "2.9.2"
+
+libraryDependencies ++= Seq(
+	"com.typesafe.akka" % "akka" % "2.0.2",
+	"com.typesafe.akka" % "akka-actor" % "2.0.2",
+	"org.specs2" %% "specs2" % "1.12.4.1" % "test")
+
+resolvers ++= Seq("snapshots", "releases").map(Resolver.sonatypeRepo)])
+buildFile.close
+gitIgnore = File.new(dirName + "/.gitignore", "w")
+gitIgnore.puts("*.class
+*.#*
+*#
+*~
+*.lic
+.idea/
+lib_managed/
+fetch.iml
+target/
+project/plugins/src_managed/
+project/boot/
+project/plugins/target/
+project/plugins/project/
+project/plugins/lib_managed/
+project/build/target/
+out/
+logs/
+*.csv
+*.iml
+*.swp
+*.iws
+*.ipr
+*.classpath
+*.project")
+gitIgnore.close
+
+
+# require 'fileutils'
+#  
+#  class Bub::Sbt < Thor
+#  	 include Thor::Actions
+#  
+#  	desc "template", "creates a sbt folder with akka and specs2 depenedency"
+#  	def template(dictName)
+#  		FileUtils::mkdir_p dictName + '/src/main/scala'
+#  	FileUtils::mkdir_p dictName + '/src/test/scala'
+#  		FileUtils::mkdir_p dictName + '/project'
+#  		buildFile = File.new(dictName + "/build.sbt", "w")
+#  		buildFile.puts(%Q[name := "#{dictName}"
+#  
+#  version:= "1.0"
